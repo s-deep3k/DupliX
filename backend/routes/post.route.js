2e186301f0b4b0a78950f41776e6b49c913983ad
s-deep3k <sankhadeep.genx@gmail.com>
2024-07-17 19:31:42 +0530

    getFollwing, GetUserPosts cholche

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,13 @@
 import e from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
+import { getAllPosts, getLikedPosts, createPost, likeUnlikePost, commentOnPost, deletePost } from "../controllers/post.controller.js";
 
 const postRouter = e.Router()
 
 postRouter.get('/all',protectRoute,getAllPosts)
 postRouter.get('/likes/:id',protectRoute,getLikedPosts)
+postRouter.get('/following',protectRoute,getFollowingPosts)
+postRouter.get('/user/:username',protectRoute,getUserPosts)
 postRouter.post('/create',protectRoute,createPost)
 postRouter.post('/like/:id',protectRoute,likeUnlikePost)
 postRouter.post('/comment/:id',protectRoute,commentOnPost)
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
+import { followUnfollowUser, getUserProfile, suggestedProfiles, updateUserProfile } from "../controllers/user.controller.js";
 
 const userRouter = Router()
 
