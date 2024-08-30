47b8218be4b3f402889eb7fc0705a7b73101780a
s-deep3k <sankhadeep.genx@gmail.com>
2024-08-31 04:12:13 +0530

    build commands and paths resolved. the end probably

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
+import path from 'path'
 import authRouter from './routes/auth.route.js'
 import { connectMongoDB } from './db/connectDB.js'
 import {v2 as cloudinary} from 'cloudinary'
@@ -9,6 +10,7 @@ import postRouter from './routes/post.route.js'
 import notificationRouter from './routes/notification.route.js'
 
 const app = express()
+const port = process.env.PORT || 5000
 dotenv.config()
 cloudinary.config({
     api_key:process.env.CLOUDINARY_API_KEY,
@@ -16,15 +18,21 @@ cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME
 })
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({limit:'5mb'}))
 app.use(urlencoded({extended:true}))
+const __dirname = path.resolve()
 
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/post",postRouter)
 app.use("api/v1/notification", notificationRouter)
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server running on Port ${process.env.PORT}`);
+    app.get("*",(req,res)=>res.sendFile(path.resolve(__dirname,"frontend","dist","index.html")))
+}
+
+app.listen(port,()=>{
+    console.log(`Server running on Port ${port}`);
     connectMongoDB()
 })
